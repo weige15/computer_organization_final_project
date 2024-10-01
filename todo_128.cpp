1d064188660ed0e0afee88f5f09f1c5f686b79dc
weige15 <56604491+weige15@users.noreply.github.com>
2024-10-01 14:15:02 +0800

    matrix transpose analysis using gem5
    methods includes: Blocking + unrolling +loop  interchange + prefetch

---
+++ todo_128.cpp
@@ -0,0 +1,38 @@
+﻿// Example program
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <stdint.h>
+#include <bits/stdc++.h>
+#include <immintrin.h>
+#define M 144
+
+using namespace std;
+
+
+void transpose(const uint64_t a[M][M], uint64_t b[M][M]) {
+    //===================only modify it this region=================================
+
+
+    const int tileSize = 1024; // Size of the tiles
+    const int unrollFactor = 12; // Number of loop iterations to process per iteration
+    for (int ii = 0; ii < M; ii += tileSize) {
+        for (int jj = 0; jj < M; jj += tileSize) {
+            for (int i = ii; i < min(ii + tileSize, M); i += unrollFactor) {
+                for (int j = jj; j < min(jj + tileSize, M); j++) {
+                    for (int k = 0; k < unrollFactor; k++) {
+                        if (i + k < M) {
+                            b[j][i + k] = a[i + k][j];
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+
+
+    //==============================================================================
+}
---
+++ todo_64.cpp
@@ -0,0 +1,50 @@
+﻿// Example program
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <stdint.h>
+#include <bits/stdc++.h>
+#include <immintrin.h>
+#define M 136
+
+
+using namespace std;
+
+
+
+
+void transpose(const uint64_t a[M][M], uint64_t b[M][M]) {
+	//===================only modify in here===============================================
+
+
+
+
+
+	int tileSize = 256; // Size of the tiles
+	const int unrollFactor = 8; // Number of loop iterations to process per iteration
+
+	for (int ii = 0; ii < M; ii += tileSize) {
+        for (int jj = 0; jj < M; jj += tileSize) {
+            for (int j = jj; j < min(jj + tileSize, M); j += unrollFactor) {
+                // Prefetch the next tile of matrix a
+                _mm_prefetch((char*)&a[ii + tileSize][j], _MM_HINT_T0);
+
+                for (int i = ii; i < min(ii + tileSize, M); i++) {
+                    for (int k = 0; k < unrollFactor; k++) {
+                        if (j + k < M) {
+                            b[j + k][i] = a[i][j + k];
+                        }
+                    }
+                }
+
+                // Prefetch the next set of elements from matrix a
+                _mm_prefetch((char*)&a[ii][j + unrollFactor], _MM_HINT_T0);
+            }
+        }
+    }
+
+
+
+
+    // =======================================================================================
+}
